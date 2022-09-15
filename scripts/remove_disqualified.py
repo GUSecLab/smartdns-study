c684acd8673f3b06253304f60576cd83006986e4
Rahel Fainchtein <rahel.fainchtein@gmail.com>
2022-09-14 22:07:52 -0400

    adding scripts folder

---
+++ scripts/remove_disqualified.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# coding=utf8
+# -*- coding: utf8 -*-
+
+# vim: set fileencoding=utf8 :
+
+
+import pandas as pd, numpy as np
+from os import getcwd as os_getcwd
+
+
+# Removal of disqualified responses from Prolific participants.
+def remove_disqualified_responses(prolificPrescreen, proMainQuant):
+    toKeep = prolificPrescreen['Q2.6']!='I have never had nor used a SmartDNS account'
+    # Q2.6: Which of the following types of Smart DNS accounts have you had or used \n\n(including any accounts you currently have/use)?\n\n(Select all that apply.)
+    #       Note: 'I have never had nor used a SmartDNS account' was an exclusive option. 
+
+    qualifies = prolificPrescreen['Q2.3'].apply(lambda x: x=='Yes')
+    #Q2.3: Do you currently use Smart DNS, or have you done so in the past?
+    
+    #Remove qualitative question fields and responses from participants who didn't meet qualification requirements from prescreen
+    prolificPrescreen = prolificPrescreen[['PROLIFIC_PID','random_id','Q2.1','Q2.3','Q2.4','Q2.6','Q9','Q9_15_TEXT','Q3.1','Q3.2','Q13.1','Q13.2','Q13.3','Q13.4','Q13.5','Q13.5_4_TEXT','Q13.6']].where(qualifies & toKeep)
+    matchRestriction = prolificPrescreen['PROLIFIC_PID'].apply(lambda x: x in set(proMainQuant['PROLIFIC_PID']))
+    prolificPrescreen = prolificPrescreen.where(matchRestriction)
+    matchRestriction = proMainQuant['PROLIFIC_PID'].apply(lambda x: x in set(prolificPrescreen['PROLIFIC_PID']))
+    proMainQuant = proMainQuant.where(matchRestriction)
+    return (prolificPrescreen, proMainQuant)
+
+
+#extract mapping of dataFrame keys (e/g 'Q2.1')
+# to the question to which they're referring (results returned as a dict).
+def get_key_mapping(dataFrame):
+    frameKeys = dataFrame.keys()
+    numKeys = len(frameKeys)
+    mapDict = dict()
+    for i in range(numKeys):
+        mapDict[frameKeys[i]] = dataFrame[frameKeys[i]][0]
+    return mapDict
+
+
+
+def parse_inputs():
+    desc = "remove_disqualified.py merges data collected in the Prolific prescreen survey with that of the main survey and removes entries for prolific participants that were previously included but ultimately determined not to qualify to participate in this study."
+    parser = ArgumentParser(description=desc)
+    parser.add_argument('-p',
+        dest='prescreen_file',
+        type=str,
+        default='data/SDNS-ProlificPrescreen_12July21.csv',
+        help='raw data file to be read in. Default: data/SDNS-ProlificPrescreen_12July21.csv (script assumed to be running from main project dir)')
+    parser.add_argument('-m',
+        dest='main_survey_file',
+        type=str,
+        default='data/SDNS-ProlificMain_12July21.csv',
+        help='Input data file containing responses to the main survey from Prolific participants. Default: data/SDNS-ProlificMain_12July21.csv (assumed to be running from main project dir)')
+    parser.add_argument('-o',
+            dest='outfile',
+            type=str,
+            default=os.getcwd()+'/prolific_datafile.csv') 
+    return parser.parse_args()
+
+
+def main():
+    params = parse_inputs()
+    prolificPrescreen = pd.read_csv(params.prescreen_file)
+    prolificMain = pd.read_csv(params.main_survey_file)
+    piiCols = ['IPAddress','RecipientLastName',
+                'RecipientFirstName', 'RecipientEmail',
+                'LocationLatitude', 'LocationLongitude',
+                'Status']
+    prolificPrescreen.drop(columns=piiCols, inplace=True)
+    prolificMain.drop(columns=piiCols, inplace=True)
+
+    # Note: For data from Prolific participants the Prolific_PID field 
+    # was used to map records collected in the prescreen survey to 
+    # reponses from the same participants given in the main survey.
+    # Once all Prolific data had been assembled, the Prolific_PID field was deleted. 
+
+    #dataFrame.drop(1,inplace=True)#drop qualtrics column IDs
+
+    prescreenNoNeed = ['StartDate','EndDate',
+                 'Progress','UserLanguage',
+                 'Duration (in seconds)','Finished',
+                 'DistributionChannel','urlPart',
+                 'idURL','Q1.3',
+                 'Q1.4','Q1.5','Rejected',
+                 'RecordedDate','ExternalReference',
+                 'SESSION_ID','STUDY_ID']
+
+    # Participants were not allowed to proceed w/the survey unless 
+    # they indicated they were 18 or older (Q1.3-prescreen, Q114-Main), 
+    # they had and understood the informed consent (Q1.4-prescreen, Q113-Main),
+    # and they consented to participate in the study (Q1.5-prescreen, Q112-Main).
+   
+    # Responses to the prescreen where it was determined that the respondent did not qualify for the main survey
+    # were marked with the 'Rejected' field, and only responses that were not marked with this field (i.e. where it was null) were exported. 
+
+    mainNoNeed = ['StartDate','EndDate',
+           'Progress','UserLanguage',
+           'Duration (in seconds)','Finished',
+           'DistributionChannel', 'urlPart',
+           'idURL','Q114',
+           'Q113','Q112',
+           'Low_quality','RecordedDate',
+           'ResponseId', 'ExternalReference',
+           'STUDY_ID', 'SESSION_ID']
+
+    # Responses to the main survey determined to be of low quality (e.g. participant did not give descriptive answers)
+    # were marked with the 'Low_quality' field, and only responses that were not marked with this field (i.e. where it was null) were exported. 
+
+    prolificPrescreen.drop(columns=prescreenNoNeed, inplace=True)
+    prolificMain.drop(columns=mainNoNeed, inplace=True)
+    
+    #get mapping dicts
+    prescreenMap = get_key_mapping(prolificPrescreen)
+    mainMap = get_key_mapping(prolificMain)
+    prolificPrescreen.drop([0,1],inplace=True)#remove field-desc from dataFrame - use prescreenMap instead
+    prolificMain.drop([0,1],inplace=True)#remove field-desc from dataFrame - use mainMap instead
+
+
+    #split off qualitative question responses from quantitative ones. (This script only handles quantitative ones):
+    #The following questions are qualitative:
+    # Prescreen:
+    # Q2.2: What are Smart DNS services primarily used for?
+    # Q3.3: To the best of your knowledge, explain how DNS works by describing the steps taken by your computer
+    #       when you navigate to a website like http://www.example.com
+    
+    # Note: Q107 is an attention check. If a participants failed it, we manually reviewed their response to assess its quality.
+    #       All 5 such responses to the prescreen were otherwise of good quality and were therefore included.  
+
+    
+    # Main Survey:
+    # Q4.6 Please explain why you think Smart DNS can affect your security and privacy in the way(s) you indicated.
+    # Q6.2 What was your main goal in using a Smart DNS service?
+    # Q6.4 Like other services Smart DNS services come with a specific set of strengths and weaknesses.
+    #      Given that Smart DNS services are not the only offerings capable of unblocking geo-fenced (geoblocked) websites 
+    #      (for example some VPN services can do this as well), what motivated you to specifically use Smart DNS?
+    # Q9.2: Please describe your view on the overall trustworthiness of Smart DNS.
+    # Q10.3: If previously blocked by a content provider when using Smart DNS: How do you think they determined you were using Smart DNS?
+    # Q11.3: Please explain your prior response: [Using Smart DNS to access content blocked in my geographic region is/is not ethical]
+    # Q11.5: Please explain your prior response: [Using Smart DNS to access content blocked in my geographic region is/is not legal]
+    
+    # prescreenQual = prolificPrescreen[['Q2.2','Q3.3']]
+    # mainQual = prolificMain[['Q4.6','Q6.2','Q6.4','Q9.2','Q10.3','Q11.3','Q11.5']]
+    prolificPrescreen.drop(columns=['Q2.2','Q3.3'],inplace=True)
+    prolificMain.drop(columns=['Q4.6','Q6.2','Q6.4','Q9.2','Q10.3','Q11.3','Q11.5'],inplace=True)
+
+    # Remove remaining disqualified responses:
+    (prolificPrescreen, prolificMain) = remove_disqualified_responses(prolificPrescreen, prolificMain)
+
+    # Align responses in both dataFrames and merge 
+    prolificMerged = prolificPrescreen.dropna(how='all').merge(prolificMain.dropna(how='all'),how='inner', on='PROLIFIC_PID')    
+    
+    #remove responses where participants used a provider that has never offered SDNS, and does not currently offer it.
+    prolificMerged = prolificMerged['Q9_15_TEXT'].apply(lambda x: x not in ['KutoVpn','OperaVPN','protonvpn','Mullvad'])
+    prolificMerged.to_csv(path_or_buf=params.outfile)
+
+
+
+
+if __name__ =='__main__':
+        main()
+
---
+++ scripts/scrub_records.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+    oficPrescreen = pd.read_csv('data/SDNS-ProlificPrescreen_12July21.csv')         default='data/')
+# coding=utf8
+# -*- coding: utf8 -*-
+
+# vim: set fileencoding=utf8 :
+
+
+
+import json
+import errno
+import re
+import numpy as np
+import pandas as pd
+
+from argparse import ArgumentParser
+from datetime import datetime
+from os import mkdir as os_mkdir,scandir as os_scandir
+
+
+
+#extract mapping of dataFrame keys (e/g 'Q2.1')
+# to the question to which they're referring (results returned as a dict).
+def get_key_mapping(dataFrame):
+    frameKeys = dataFrame.keys()
+    numKeys = len(frameKeys)
+    mapDict = dict()
+    for i in range(numKeys):
+        mapDict[frameKeys[i]] = dataFrame[frameKeys[i]][0]
+    return mapDict
+
+#Helper for get_service_use()
+#Map Q2.3 (currently/prev. used SDNS) to boolean
+#filter Q9 (Which services used/considering) to distinguish consideration vs use.
+def filter_by_sdns_use(useCol, services):
+     useFilter = useCol.map({'Yes':True, 'No':False},na_action='ignore')
+     useFilter.fillna(False,inplace=True)
+     serviceUse = services[useFilter]
+     considers = services[useFilter.apply(lambda x: not(x))]
+     return (serviceUse,considers)
+
+# How many participants use/used SDNS and how many are considering?
+# Usage breakdown by service (useByService), number of services used
+# breakdown by services considered (with double-counting), number of services considered
+def get_service_use(useCol,services):
+    (serviceUse, considers) = filter_by_sdns_use(useCol,services)
+    useByService = serviceUse.str.split(pat=',').explode().value_counts()
+    useByServNorm = serviceUse.str.split(pat=',').explode().value_counts(normalize=True)
+    numServicesUsed = serviceUse.str.split(pat=',').map(lambda x: len(x)).value_counts()
+    numServUsedNorm = serviceUse.str.split(pat=',').map(lambda x: len(x)).value_counts(normalize=True)
+    
+    consByService = considers.str.split(pat=',').explode().value_counts()
+    consByServNorm = considers.str.split(pat=',').explode().value_counts(normalize=True)
+    numServConsidered = considers.str.split(pat=',').map(lambda x: len(x)).value_counts()
+    numServConsNorm = considers.str.split(pat=',').map(lambda x: len(x)).value_counts(normalize=True)
+    
+    ret = {
+            'useByService':useByService, 'useByServNorm':useByServNorm,
+            'numServicesUsed':numServicesUsed, 'numServUsedNorm':numServUsedNorm,
+            'consByService':consByService, 'consByServNorm': consByServNorm,
+            'numServConsidered':numServConsidered, 'numServConsNorm':numServConsNorm
+            }
+    return ret
+
+
+# Using Smart DNS when I browse the Internet provides additional security(' ')/privacy(' .1') 
+def get_sec_priv_impressions(nPrivSecLikert):
+    ret = {
+            'moreBrowsingSec': nPrivSecLikert[' '].value_counts(),
+            'browseSecNorm': nPrivSecLikert[' '].value_counts(normalize=True),
+            'moreBrowsingPriv': nPrivSecLikert[' .1'].value_counts(),
+            'browsePrivNorm': nPrivSecLikert[' .1'].value_counts(normalize=True)
+            }
+    return ret
+
+
+#Handle subframes with likert value responses.
+def process_likert(subframe,mapDict):
+    retDict = dict()
+    for col in subframe.keys():
+        if col == 'random_id':
+              continue
+        colVal = mapDict[col]
+        valDist = subframe[col][1:].value_counts()
+        normValDist = subframe[col][1:].value_counts(normalize=True)
+        retDict[col] = {
+                           'description': colVal,
+                           'distribution':valDist, 'normalized_dist':normValDist
+                       }
+    return retDict
+
+
+
+# Get the relative distributions of options selected
+# for each entry in a select many column
+def select_many_dist(col,titleSuffix):
+    colLists = col.str.split(',')
+    lengths = colLists.apply(lambda x: len(x) if isinstance(x,list) else np.nan)
+    lengthDist = lengths.value_counts(dropna=True)
+    distributionDict = dict()
+    subsetSizes = dict()
+    maxVal = lengths.dropna().max()
+    for i in range(1,int(maxVal)+1):
+        subsetI = colLists.apply(lambda x: x if isinstance(x,list) and len(x)==i else np.nan).dropna()
+        prevalenceDist = subsetI.explode().value_counts()
+        distributionDict[str(i)+titleSuffix] = prevalenceDist
+    return (lengthDist, distributionDict)
+
+
+# Q7.1: Which other services do SDNS providers offer (if any)? 
+# Q7.2: Were participants mainly looking to use SDNS when they signed up w/their (respective) service provider(s)?
+# Q7.4: Which (if any) of the additional services (other than SDNS) did participants use?
+def analyze_other_offerings(otherServices,mapDict):
+    offersAndMotives = process_likert(otherServices[['Q7.1','Q7.2']],mapDict)
+    (countsByNumOffered,offerPrevalence) = select_many_dist(otherServices['Q7.3'][1:],' service(s)') 
+    (countsByNumUsed,usePrevalence) = select_many_dist(otherServices['Q7.4'][1:],'service(s)')
+    ret =  (countsByNumOffered ,offerPrevalence ,
+            countsByNumUsed, usePrevalence
+            )
+    return ret
+
+
+
+# Removal of disqualified responses from Prolific participants.
+def remove_disqualified_responses(prolificPrescreen, proMainQuant):
+    toKeep = prolificPrescreen['Q2.6']!='I have never had nor used a SmartDNS account'
+    qualifies = prolificPrescreen['Q2.3'].apply(lambda x: x=='Yes')
+    prolificPrescreen = prolificPrescreen[['PROLIFIC_PID','random_id','Q2.1','Q2.2','Q2.3','Q2.4','Q2.6','Q9','Q9_15_TEXT','Q3.1','Q3.2','Q3.3','Q13.1','Q13.2','Q13.3','Q13.4','Q13.5','Q13.5_4_TEXT','Q13.6']].where(qualifies & toKeep)
+    matchRestriction = prolificPrescreen['PROLIFIC_PID'].apply(lambda x: x in set(proMainQuant['PROLIFIC_PID']))
+    prolificPrescreen = prolificPrescreen.where(matchRestriction)
+    matchRestriction = proMainQuant['PROLIFIC_PID'].apply(lambda x: x in set(prolificPrescreen['PROLIFIC_PID']))
+    proMainQuant = proMainQuant.where(matchRestriction)
+    return (prolificPrescreen, proMainQuant)
+
+
+
+def parse_inputs():
+    desc = "scrub_records.py removes PII fields from qualtrics data (based on field names) and splits quantitative and qualitative results into two separate data files."
+    parser = ArgumentParser(description=desc)
+    parser.add_argument('-f',
+                        dest='infile',
+                        type=str,
+                        default='data/SDNS_RedditData_raw.csv',
+                        help='raw data file to be read in and scrubbed of PII. Default: data/SDNS_RedditData_raw.csv (assumed to be running from main project dir)')
+    parser.add_argument('-d',
+                        dest='newDirs',
+                        action='store_true',
+                        help='If specified, create new directories for the quantitative and qualitative data if they don\'t already exist. NOTE: directory names required if this option is specified. Default behavior: output files to the current working directory.')
+    parser.add_argument('-qn',
+                        dest='quantDir',
+                        nargs='?',
+                        const='quantitative_metrics',
+                        help='Path to directory for quantitative output data files. Flag is required if -d is specified. Path value defaults to the current_working_dir/quantitative_metrics ')
+    parser.add_argument('-ql',
+                        dest='qualDataDir',
+                        nargs='?',
+                        help='Path to directory for qualitative output files. Flag is required if -d is specified. Path value defaults to the current_working_dir/qual_data_metrics')
+    return parser.parse_args()
+
+
+
+def main():
+    params = parse_inputs()
+    dataFrame = pd.read_csv(params.infile)
+    piiCols = ['IPAddress','RecipientLastName',
+                'RecipientFirstName', 'RecipientEmail',
+                'LocationLatitude', 'LocationLongitude',
+                'Status']
+    dataFrame.drop(columns=piiCols, inplace=True)
+    # Note: For data from Prolific participants the Prolific_PID field 
+    # was used to map records collected in the prescreen survey to 
+    # reponses from the same participants given in the main survey.
+    # Once all Prolific data had been assembled, the Prolific_PID field was deleted. 
+
+    dataFrame.drop(1,inplace=True)#drop qualtrics column IDs
+
+    noneedCols = ['Progress','UserLanguage',
+                  'DistributionChannel','urlPart',
+                  'idURL','Q1.3',
+                  'Q1.4','Q1.5']#'EndDate','StartDate']#specifically removed from Reddit data
+    
+    # utm fields were mainly used to determine the most effective means of participant recruitment through Reddit
+    # and were not needed for additional analysis. These fields were not present in data collected via Prolific. 
+    # dataFrame.drop(columns=[ 'Source', 'utm_source', 'utm_medium', 'utm_campaign'], inplace=True)
+
+    mapDict = get_key_mapping(dataFrame)
+    dataFrame.drop(0,inplace=True)#remove field-desc from dataFrame - use mapDict instead
+    
+    # sanity check: Review the names of the fields to ensure initial PII has been scrubbed. 
+    print(dataFrame.keys()[:20])
+    print(dataFrame.keys()[60:])
+   
+
+
+    # Split off smaller dataFrame (copies) to perform analysis.
+   
+    # demographics dataFrame
+    demographics = dataFrame[['random_id','Q13.1','Q13.2','Q13.3','Q13.4','Q13.5','Q13.5_4_TEXT','Q13.6']]
+    # Q13.1: What is your age? 
+    # Q13.2: What is the highest level of school you have completed or the highest degree you have received?
+    # Q13.3: In which country do you live?
+    # Q13.4: What is your nationality?
+    # Q13.5: What is the gender to which you most closely identify?
+    # Q13.6: What is your annual income in US Dollars?
+
+    demogStats = process_likert(demographics,mapDict) 
+
+    otherServices = dataFrame[['Q7.1','Q7.2','Q7.3','Q7.4']]
+    # Q7.1: Which other services do SDNS providers offer (if any)? 
+    # Q7.2: Were participants mainly looking to use SDNS when they signed up w/their (respective) service provider(s)?
+    # Q7.3: Which other services does/did your Smart DNS provider offer? (Select all that apply)
+    # Q7.4: Which (if any) of the additional services (other than SDNS) did participants use?
+
+
+    # perform analysis
+    (countsByNumOffered,offerPrevalence, countsByNumUsed, usePrevalence) = analyze_other_offerings(otherServices,mapDict) 
+
+    #Quantitative analysis fields
+
+    #Q6.5 Factors 
+
+    # SDNS usage:
+    
+
+
+if __name__ =='__main__':
+    main()
